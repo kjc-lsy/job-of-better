705f7f17c2fb3820d118f4c9c1a39a5d065fdb0c
dealim <dealimmmm@gmail.com>
2024-01-18 21:32:37 +0900

    chore: frontend 추가

---
+++ src/main/java/site/dealim/jobseeker/controller/HelloController.java
@@ -0,0 +1,13 @@
+package site.dealim.jobseeker.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/api/test")
+    public String hello() {
+        return "테스트입니다.";
+    }
+}
