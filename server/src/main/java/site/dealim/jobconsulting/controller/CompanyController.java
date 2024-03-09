2593270efe6cf27420a7d07710605dac19c42213
dealim <dealimmmm@gmail.com>
2024-03-09 22:17:16 +0900

    fix: server 실행 오류 수정
    - CCL-Mapper 설정을 하지 않아서 실행 되지 않았음

--- server/src/main/java/site/dealim/jobconsulting/controller/CompanyController.java
+++ server/src/main/java/site/dealim/jobconsulting/controller/CompanyController.java
@@ -1,15 +1,13 @@
 package site.dealim.jobconsulting.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import site.dealim.jobconsulting.service.CompanyService;
 
-@Controller
 @RestController
 public class CompanyController {
-    CompanyService adminService;
-    @PostMapping("comCoverLetterSave")
+    private CompanyService adminService;
+    @PostMapping("/com-cl-save")
     public String comCoverLetterSave() {
         return adminService.comCoverLetterSave();
     }
