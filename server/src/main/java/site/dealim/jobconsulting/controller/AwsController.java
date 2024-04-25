910cb1c5f0442cebd4e355df3637bad58dc83e91
leesy94 <zen294123@gmail.com>
2024-04-25 11:03:36 +0900

    feat : aws controller 수정

--- server/src/main/java/site/dealim/jobconsulting/controller/AwsController.java
+++ server/src/main/java/site/dealim/jobconsulting/controller/AwsController.java
@@ -1,10 +1,15 @@
 package site.dealim.jobconsulting.controller;
 
 import com.amazonaws.services.s3.model.AmazonS3Exception;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +21,8 @@ import java.util.List;
 
 @RestController
 @Log4j2
-@RequestMapping("/api")
+@Tag(name = "AWS 파일 업로드", description = "AWS Controller")
+@RequestMapping("/api/files")
 public class AwsController {
     private final AwsService awsService;
 
@@ -25,9 +31,20 @@ public class AwsController {
         this.awsService = awsService;
     }
 
-    @PostMapping(path = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-    public ResponseEntity<List<String>> uploadFile(List<MultipartFile> multipartFiles){
-        return ResponseEntity.ok(awsService.uploadFile(multipartFiles));
+    @PostMapping(path = "/upload",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "S3 파일 업로드" , description = "multifile list를 s3에 업로드하여 filename list 반환")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "success"),
+            @ApiResponse(responseCode = "500", description = "internal server error"),
+            @ApiResponse(responseCode = "400", description = "bad request"),
+            @ApiResponse(responseCode = "404", description = "not found")
+    })
+    public ResponseEntity<?> uploadFile(List<MultipartFile> multipartFiles) throws Exception{
+        log.info("AWS file Upload");
+        List<String> fileNameList = awsService.uploadFile(multipartFiles);
+        return ResponseEntity.<String>ok().body(fileNameList);
     }
 
     @GetMapping("/download/{fileName}")
